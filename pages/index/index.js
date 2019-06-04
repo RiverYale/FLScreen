4e38ef978d1b52047ed8e2326c09499cdf240223
YaleRiver <1181065460@qq.com>
2019-06-04 23:27:46 +0800

    删库跑路

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,25 +12,39 @@ Page({
             },
         ]
     },
-    
+
     onLoad: function () {
-      var devices = dataBase.getDeviceList();
-      var that = this;
-      devices.forEach(function(item,index){//这时候应该更新一下看设备在不在线
-        dataBase.queryState(item).then(function(res){//里面包了一个promise
-          console.log(res);
-          devices[index].online = res.data.online;//更新一下在线情况
-          that.setData({
-            dataArray:devices
-          })
+        var devices = dataBase.getDeviceList();
+        var that = this;
+        devices.forEach(function (item, index) {//这时候应该更新一下看设备在不在线
+            dataBase.queryState(item).then(function (res) {//里面包了一个promise
+                console.log(res);
+                devices[index].online = res.data.online;//更新一下在线情况
+                that.setData({
+                    dataArray: devices
+                })
+            })
         })
-      })
-      this.setData({
-        dataArray:devices,
-      })
+        this.setData({
+            dataArray: devices,
+        })
     },
 
-    onPullDownRefresh: function() {
+    onPullDownRefresh: function () {
+        var devices = dataBase.getDeviceList();
+        var that = this;
+        devices.forEach(function (item, index) {//这时候应该更新一下看设备在不在线
+            dataBase.queryState(item).then(function (res) {//里面包了一个promise
+                console.log(res);
+                devices[index].online = res.data.online;//更新一下在线情况
+                that.setData({
+                    dataArray: devices
+                })
+            })
+        })
+        this.setData({
+            dataArray: devices,
+        })
         wx.stopPullDownRefresh()
     },
 
